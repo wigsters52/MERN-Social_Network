5a240f9a23cf13a4620f46a2f94d7735f57c3809
Ryan Wiggins <ryanwiggins52@gmail.com>
2021-04-06 13:50:11 -0400

    added router

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.send('Api running')
 })
 
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
