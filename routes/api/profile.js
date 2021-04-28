07278c7183cf119c514262ef6f2be4c49159d3fd
Ryan Wiggins <ryanwiggins52@gmail.com>
2021-04-28 08:40:50 -0400

    get github repos for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const axios = require('axios')
+const request = require('request')
 const config = require('config')
 const router = express.Router()
 const auth = require('../../middleware/auth')
@@ -213,6 +213,26 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   }
 });
 
+router.get('/github/:username', async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client)id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js'}
+    }
+    request(options, (error, response, body) => {
+      if(error) console.error(error)
 
+      if(response.statusCode !== 200){
+        return res.status(404).json({msg: 'No github profile found'})
+      }
+      res.json(JSON.parse(body))
+    })
+  } catch(err) {
+    console.error(err.message)
+    return res.status(500).json({ msg: 'Server error' })
+
+  }
+});
 
 module.exports = router
